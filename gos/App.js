3d3c101895c98b21b3ae51a48ea32c6df63b1dda
Aron M <aronms17@ru.is>
2020-02-13 22:41:06 +0000

    marker on map..

--- gos/App.js
+++ gos/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <MapView style={styles.mapStyle} />
+        <MapView
+        style={styles.mapStyle}
+        initialRegion={{
+        latitude: 63.4347866,
+        longitude: -20.2844343,
+        latitudeDelta: 0.095,
+        longitudeDelta: 0.0921}}>
+        <MapView.Marker
+            coordinate={{latitude: 63.4347866, longitude: -20.2844343}}
+            title={'Deneme'}
+        />
+    <MapView.Marker
+       description={'güzel mekan'}
+       coordinate={{latitude: 38.4555, longitude: 27.1129}}
+       title={'Deneme'}/>
+</MapView>
+        
       </View>
     );
   }
