4104e240aea286e29848ff1d6c8de1839529ac00
baldvinw <baldvinw15@ru.is>
2020-03-06 14:59:12 +0000

    scripta

---
+++ gos/script/proj4convert.js
@@ -0,0 +1,35 @@
+var proj4 = require("proj4");
+var json = require("./Heimaey-hus-fyrirgos.json")
+
+//define WGS 84
+proj4.defs('WGS84', "+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees");
+
+//define EPSG:3057
+proj4.defs("EPSG:3057","+proj=lcc +lat_1=64.25 +lat_2=65.75 +lat_0=65 +lon_0=-19 +x_0=500000 +y_0=500000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs");
+
+//read json
+var hus = [];
+
+for(var i=0; i<3; i++) {
+    var tmp = [];
+    for(var j=0; j<json.features[i].geometry.coordinates[0][0].length; j++) {
+        var floatarr = [];
+        //console.log(Number.parseFloat(json.features[i].geometry.coordinates[0][0][j][0]).toFixed(16))
+        //console.log(Number.parseFloat(json.features[i].geometry.coordinates[0][0][j][1]).toFixed(16))
+        floatarr.push(Number.parseFloat(Number.parseFloat((json.features[i].geometry.coordinates[0][0][j][0]).toFixed(16))));
+        floatarr.push(Number.parseFloat(json.features[i].geometry.coordinates[0][0][j][1]));
+        console.log(floatarr)
+        //tmp.push(proj4('EPSG:3057', 'WGS84', parsefloat));
+        
+    }
+    hus.push(tmp);
+    
+};
+
+console.log(hus);
+
+
+
+
+//convert
+//proj4('EPSG:3057', 'WGS84', [ 436841.734249254164752, 326900.030814741156064 ]);
