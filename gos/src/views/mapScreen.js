250c63375907b4140eb7fbeb282f26eee91d82d8
baldvinw <baldvinw15@ru.is>
2020-03-27 19:04:04 +0000

    lookið

--- gos/src/views/mapScreen.js
+++ gos/src/views/mapScreen.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import MapView, { Marker, Overlay, UrlTile, Polygon } from 'react-native-maps';
-import { Alert, StyleSheet, Text, View, Dimensions, Image, FlatList, setNativeProps } from 'react-native';
+import { Alert, TextInput, StyleSheet, Text, View, Dimensions, Image, FlatList, setNativeProps } from 'react-native';
 // import data from './src/houses.json';
 import mapjson from '../json/mapstyle.json';
 import prufupoly from '../../script/jsonfile.json';
 import CustomPolygon from '../components/CustomPolygon';
 
 export default class App extends React.Component {
-
 constructor(props) {
   super(props);
-
   this.state = {
     husColor: null /* you can use isIOS() ? null : 'rgba(60, 165, 255, 0.2)'*/,
     goturColor: null /* you can use isIOS() ? null : 'rgba(60, 165, 255, 1)'*/,
   };
+}
+
+
+static navigationOptions = {
+    title: '',
+    headerTransparent: true,
+    headerStyle: {
+      backgroundColor: 'transparent',
+      elevation: 0,
+      shadowOpacity: 0,
+      borderBottomWidth: 0,
+    //   height: 40,
+    },
+    headerTintColor: 'red',
+      headerTitleStyle: {
+      fontWeight: 'bold',
+    },
 }
     
   poly1 = [
@@ -58,44 +73,52 @@ navigateHouse(houseid) {
   render() {
     const {goturColor, husColor} = this.state;
     return (
-      <View style={styles.container}>
-        <MapView
-        
-        style={styles.mapStyle}
-        provider={"google"}
-        customMapStyle={mapjson}
-        initialRegion={{
-        latitude: 63.4347866,
-        longitude: -20.2844343,
-        latitudeDelta: 0.095,
-        longitudeDelta: 0.0921}}>
+    <View style={styles.component}>
+        <View style={styles.container}>
+            <MapView
+            
+            style={styles.mapStyle}
+            provider={"google"}
+            customMapStyle={mapjson}
+            initialRegion={{
+            latitude: 63.4347866,
+            longitude: -20.2844343,
+            latitudeDelta: 0.095,
+            longitudeDelta: 0.0921}}>
 
-        {/* þarf að refresha til að litirnir komi */}
+            {/* þarf að refresha til að litirnir komi */}
 
-        {prufupoly.hus[0] != null && prufupoly.hus.map((hus, index) => (
-            <CustomPolygon
-              key = {hus.id}
-              coordinates={hus.coordinates}
-              fillColor={husColor}
-              tappable={true}
-              onPress={() => this.navigateHouse(hus.id)}
+            {prufupoly.hus[0] != null && prufupoly.hus.map((hus, index) => (
+                <CustomPolygon
+                key = {hus.id}
+                coordinates={hus.coordinates}
+                fillColor={husColor}
+                tappable={true}
+                onPress={() => this.navigateHouse(hus.id)}
+                />
+                ))
+            }
+
+            {prufupoly.gotur[0] != null && prufupoly.gotur.map((gata, index) => (
+                <CustomPolygon
+                key = {gata.id}
+                coordinates={gata.coordinates}
+                fillColor={goturColor}
+                />
+            ))
+            }
+            </MapView>
+            
+        </View>
+        <View style={styles.search}>
+            <TextInput
+            placeholder="Search"
+            placeholderTextColor="#dddddd"
+            style={ styles.searchInput }
+            onChangeText={value => console.log(value)}
             />
-        ))
-        }
-
-        {prufupoly.gotur[0] != null && prufupoly.gotur.map((gata, index) => (
-            <CustomPolygon
-              key = {gata.id}
-              coordinates={gata.coordinates}
-              fillColor={goturColor}
-            />
-        ))
-        }
-
-
-</MapView>
-        
-      </View>
+        </View>
+    </View>
     );
   }
 }
@@ -106,6 +129,39 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  search: {
+      flex: 1,
+      height: 90, 
+      width: Dimensions.get('window').width, 
+      backgroundColor: 'rgba(38, 38, 48, 0.5)', 
+      position: 'absolute',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      borderRadius:10,
+  },
+  naggur: {
+
+  },
+  searchInput: {
+    backgroundColor: 'white',
+    height: 35,
+    width: Dimensions.get('window').width - 30,
+    marginLeft: 15,
+    marginRight: 15,
+    marginTop: 15,
+    fontSize: 28,
+    padding: 10,
+    color: 'black',
+    borderRadius: 10,
+    fontSize: 15,
+    // borderBottomWidth: 2,
+    // borderBottomColor: 'blue'
+  },
+  component: {
+      flex:1,
+      flexDirection: 'column-reverse',
+
   },
   mapStyle: {
     width: Dimensions.get('window').width,
