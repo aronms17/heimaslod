9bbb4b285db04b83bdf3b0b6b40dbbf78132983d
Aron M <aronms17@ru.is>
2020-05-01 12:50:01 +0000

    smellt á götunafn í listanum

--- gos/src/views/allStreetScreen.js
+++ gos/src/views/allStreetScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text, View, TouchableHighlight, StyleSheet, Dimensions, FlatList, TouchableOpacity } from 'react-native';
 import { Feather, MaterialIcons  } from '@expo/vector-icons'
+import DrawerLayout from 'react-native-gesture-handler/DrawerLayout';
 // import Streets from './../components/Streets';
 import Data from '../../script/jsonfile.json';
 
@@ -27,17 +28,19 @@ export default class allStreetScreen extends React.Component {
         });
     }
 
+    navigateStreet(streetId, streetName) {
+        this.props.navigation.navigate('streetDetailScreen', {
+            streetId, streetName
+          });
+    }
+
     renderDrawer = () => {
         return (
           <View style={styles.sideMenu}>
-            <TouchableHighlight onPress={() => this.props.navigation.navigate('allStreetScreen')}>
-              <Text style={styles.sideMenuText}>Allar Götur</Text>
+            <TouchableHighlight onPress={() => {this.props.navigation.navigate('mapScreen'); this.drawer.closeDrawer();} }>
+                <Text style={styles.sideMenuText}>Kort</Text>
             </TouchableHighlight>
-            <TouchableHighlight onPress={() => this.drawer.closeDrawer()}>
-                <Text style={styles.sideMenuText}>Hehe</Text>
-            </TouchableHighlight>
-            <Text style={styles.sideMenuText}>Stillingar</Text>
-             
+            <Text style={styles.sideMenuText}>Stillingar</Text>        
           </View>
         );
       }
@@ -46,11 +49,21 @@ export default class allStreetScreen extends React.Component {
 
     render() {
         return(
-            <View style={{backgroundColor: '#1D1B1B'}}>
+            <View style={styles.container}>
+                <DrawerLayout
+                    ref={drawer => {
+                      this.drawer = drawer;
+                    }}
+                    drawerWidth={220}
+                    drawerPosition={DrawerLayout.positions.Right}
+                    drawerType='front'
+                    drawerBackgroundColor='#1D1B1B'
+                    renderNavigationView={this.renderDrawer}
+                >
                 <View style={styles.header}>
                     <TouchableHighlight
                       style={styles.burger}
-                      onPress={() => console.log('h')}>
+                      onPress={() => this.drawer.openDrawer()}>
                         <Feather name='menu' size={40} color='white'/>
                     </TouchableHighlight>
                 </View>
@@ -58,8 +71,8 @@ export default class allStreetScreen extends React.Component {
                     data={this.state.streets}
                     renderItem={({item}) => (
                         <>
-                            <TouchableOpacity style={{margin: 1}} onPress={() => console.log(item.name)}>
-                                <Text style={{fontSize: 30, color: 'white'}}>{item.name}</Text>
+                            <TouchableOpacity style={{margin: 1}} onPress={() => this.navigateStreet(item.id, item.name)}>
+                    <Text style={{fontSize: 30, color: 'white'}}>{item.name}</Text>
                             </TouchableOpacity>
                             <View>
                                 {this.state.houses[0] != null && this.state.houses.filter(house => house.streetId === item.id).map((house) => (
@@ -73,6 +86,7 @@ export default class allStreetScreen extends React.Component {
                         
                     )}
                 />
+                </DrawerLayout>
             </View>
         );
     }
@@ -83,14 +97,73 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#1D1B1B',
         justifyContent: 'center'
+        
+	},
+	headerContainer: {
+        flex: 2,
+        flexDirection: 'row',
+		alignItems: 'center',
+        justifyContent: 'center',
     },
-    header: {
+    galleryContainer: {
+        flex: 3,
+    },
+    descriptionContainer: {
+        flex: 4,
+        paddingLeft: 20,
+        paddingRight: 20
+    },
+    bottomContainer: {
+        flex: 5,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        flexDirection: 'row',
+        paddingLeft: 20,
+        paddingRight: 20
+    },
+    bottomItems: {
+        marginRight: 10
+    },
+    onlyMap: {
+        ...StyleSheet.absoluteFillObject,
+        width: 230,
+        height: 150,
+        marginTop: 15,
+        marginLeft: 15,
+        marginRight: 15,
+        position: 'relative'
+    },
+	name: {
+        fontSize: 36,
+        fontWeight: 'bold',
+		color: '#fff'
+    },
+    desc: {
+		fontSize: 16,
+        color: '#fff',
+    },
+    sideMenu: {
+        flex:1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
+        paddingTop: 50,
+        paddingBottom: 30,
+        paddingLeft: 25
+    
+      },
+      sideMenuText: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        color: 'white'
+      },
+      header: {
         width: Dimensions.get('screen').width, 
         flexDirection: 'row',
         justifyContent: 'flex-end'
-    },
-    burger: {
+      },
+      burger: {
         marginTop: 40,
         marginRight: 17
-    }
+      }
 });
