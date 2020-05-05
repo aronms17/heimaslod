d3b714e4b3caafa6032495069bf73597b29e1189
Aron M <aronms17@ru.is>
2020-05-05 20:47:36 +0000

    styles

---
+++ gos/src/styles/colors.js
@@ -0,0 +1,10 @@
+export default {
+    blue: '#29C6D6',
+    grey: '#424C58',
+    greyLight: '#AFB7C2',
+    black: '#000000',
+    okkarSvarti: '#1D1B1B',
+    white: '#FFFFFF',
+    green: '#43e01f',
+  };
+  
---
+++ gos/src/styles/styles.js
@@ -0,0 +1,57 @@
+import { StyleSheet, Dimensions } from 'react-native';
+import colors from './colors';
+
+export default StyleSheet.create({
+    map: {
+      backgroundColor: '#fff'
+    },
+    components: {
+      width: Dimensions.get('window').width,
+      height: Dimensions.get('window').height,
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+  
+    },
+    modalView: {
+      margin: 20,
+      backgroundColor: "white",
+      borderRadius: 20,
+      padding: 35,
+      alignItems: "center",
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 3.84,
+      elevation: 5
+    },
+    sideMenu: {
+      flex:1,
+      flexDirection: 'column',
+      justifyContent: 'flex-start',
+      alignItems: 'flex-start',
+      paddingTop: 50,
+      paddingBottom: 30,
+      paddingLeft: 25
+  
+    },
+    sideMenuText: {
+      fontSize: 28,
+      fontWeight: 'bold',
+      color: 'white'
+    },
+    sideMenuItem: {
+      marginBottom: 30
+    },
+    header: {
+      width: Dimensions.get('screen').width, 
+      flexDirection: 'row',
+      justifyContent: 'flex-end',
+    },
+    burger: {
+      marginTop: 40,
+      marginRight: 17,
+    }
+  });
